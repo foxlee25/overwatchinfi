bc1f3b517555f5695aeae77e67bbbd6770322edf
jzy521 <jzy521640@gmail.com>
2016-05-09 02:05:31 -0700

    init commit
    backend node framework

---
+++ Time/timeServer/DaoController.js
@@ -0,0 +1,41 @@
+/**
+ * @author Jin, Zheyang
+ */
+// var fs = require('fs')
+// fs.readFile()
+//request(url,function(){callback})
+
+/**** keep track all the dao go through DB**/
+var DBController = require('./DBController.js');
+var DaoManager = {};
+// !!! add all the dao file here
+DaoManager.UserDao = require('./Dao/UserDao.js');
+
+/**
+ *  daoFileName use to separate dao files.
+ */
+var daoController = {};
+daoController.getDao = function (daoFileName, daoMethodName, restCallback) {
+    if (daoFileName && daoMethodName) {
+        DBController.getConnection(function (DBConnection) {
+            if (DaoManager[daoFileName]) {
+                if (DaoManager[daoFileName][daoMethodName]) {
+                    console.log('DaoFile : ' + daoFileName + ' Method :' + daoMethodName + ' is success !!!');
+                    DaoManager[daoFileName][daoMethodName](DBConnection, restCallback);
+                } else {
+                    console.log('DaoFile : ' + daoFileName + ' Method :' + daoMethodName + ' do not exists!!!');
+                }
+
+            } else {
+                console.log('DaoFile ' + daoFileName + ' do not exists !!!');
+            }
+
+        });
+
+
+    }
+
+};
+
+
+module.exports = daoController;
---
+++ Time/timeServer/TimeServer.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+
+var RestController = require('./RestController.js');
+var app = express();
+app.use(cors());
+//use on ec2
+//app.listen(80);
+//use on local
+app.listen(3000, '127.0.0.1');
+app.use('/', express.static('./timeClient/'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+/**
+ * process all rest call
+ */
+app.use(RestController);
+console.log('Time Server start...');
