74472b5e2193e9368521724265e078033e651579
qixiaohui <qixiaohuihaha@gmail.com>
2016-05-16 20:27:12 -0700

    tweak hero page bg

--- Time/overwatch/Client/views/HeroCard.js
+++ Time/overwatch/Client/views/HeroCard.js
@@ -6,13 +6,11 @@ var HeroCard = React.createClass({
 	},
 	render: function(){
 		return (
-			<div className="col-sm-2">
-				<div className="card">
-				  <img id="herocard" className="card-img-top" src={"./img/hero/"+this.props.hero.imgPath} alt="Card image cap" />
-				  <div className="card-block">
+			<div className="col-sm-3">
+				<div id="cardDiv">
+		  			<img id="herocard" className="img-responsive" src={"./img/hero/"+this.props.hero.imgPath} />
 				    <p id="cardTitle" className="card-text">{this.props.hero.heroname}</p>
-				  </div>
-				</div>
+			    </div>
 			</div>
 		);
 	}
